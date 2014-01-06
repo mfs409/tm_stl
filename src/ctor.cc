a383fd84ba5f55e6f22b87bfc4ea5a3c01f3a0d8
Michael Spear <spear@cse.lehigh.edu>
2014-01-05 23:14:25 -0500

    concurrent ctor test complete

--- src/ctor.cc
+++ src/ctor.cc
@@ -58,103 +58,136 @@ void ctor_test_seq()
     delete my_list;
 }
 
+/// The list we will use for our tests
+std::list<int>* ctor_list = NULL;
+
+/// An extra array of lists, which is sometimes useful.  Note that while we
+/// treat this as per-thread, it's globally visible, so the compiler can't
+/// assume anything...
+std::list<int>* per_thread_list[256] = {NULL}; // 256 is way more than enough...
+
+/// clone the list to a local array represented by dsize, data[]
+#define COPY_LIST                                 \
+    dsize = 0;                                    \
+    for (auto i : *ctor_list)                     \
+        data[dsize++] = i
+#define CLEAR_LIST                                \
+    delete ctor_list;                             \
+    ctor_list = NULL
+
+/// compare the local array to our expected results
+#define VERIFY(testName, size, ...)                                     \
+    if (dsize != size)                                                  \
+        printf(" [%d] size did not match %d != %d\n", id, dsize, size);  \
+    else if (size > 0) {                                                \
+        int counter = 0;                                                \
+        bool good = true;                                               \
+        for (auto i : __VA_ARGS__)                                      \
+            good &= (data[counter++] == i);                             \
+        if (!good)                                                      \
+            std::cout << " ["<<id<<"] Arrays did not match for thread " << id << std::endl; \
+        else if (id == 0)                                               \
+            std::cout << " [OK] " << testName << std::endl;              \
+    }                                                                   \
+    else if (id == 0)                                                   \
+            std::cout << " [OK] " << testName << std::endl
+
 /**
  * Here is the concurrent ctor test.  It also happens to be the concurrent
  * dtor test, since there is only a basic dtor.
  */
 void ctor_test_concurrent(int id)
 {
+    // invariant: the list starts off null
+    assert(ctor_list == NULL);
+
+    // helper message
     if (id == 0) {
-        std::cout << "Testing ctor and dtor" << std::endl;
-        std::cout << "Note: we aren't testing the allocator parameter "
-                  << "of the constructors" << std::endl;
+        std::cout << std::endl
+                  << "Testing ctor and dtor" << std::endl
+                  << "(Note: we aren't testing the allocator parameter "
+                  << "of the constructors)" << std::endl;
     }
 
     // scratch space for holding data when computing whether the test
     // succeeded or failed.
-    int data[256], dsize = 0;
+    int data[32], dsize = 0;
 
     // test basic constructor (1)
     global_barrier->arrive(id);
     BEGIN_TX;
-    // ensure the list is not constructed
-    if (my_list != NULL) {
-        dsize = 256; // not 0, so we'll report an error
-    }
-    else {
-        // call the basic constructor, then iterate through the list to
-        // ensure that it is empty
-        my_list = new std::list<int>();
-        dsize = 0;
-        for (auto i : *my_list)
-            data[dsize++] = i;
-        delete my_list;
-        my_list = NULL;
-    }
+    // call the basic constructor, expect empty list
+    ctor_list = new std::list<int>();
+    COPY_LIST;
+    CLEAR_LIST;
     END_TX;
-    std::cout << "Basic ctor (1) " << (dsize == 0 ? "passed" : "failed") << std::endl;
+    VERIFY("basic ctor (1)", 0, {0});
 
     // test sized constructor (2)
     global_barrier->arrive(id);
     // make sure we'll detect errors in data[]
     for (int i = 0; i < 16; ++i)
         data[i] = 17;
-    dsize = 0;
     BEGIN_TX;
-    // ensure the list is not constructed
-    if (my_list != NULL) {
-        dsize = 256; // not 0, so we'll report an error
-    }
-    else {
-        // call the sized constructor, then iterate through the list to
-        // ensure that it has 5 zeroes
-        my_list = new std::list<int>(5);
-        dsize = 0;
-        for (auto i : *my_list)
-            data[dsize++] = i;
-        delete my_list;
-        my_list = NULL;
-    }
+    // call the sized constructor, expect {0,0,0,0,0}
+    // NB: we padded data[] to detect overflow
+    ctor_list = new std::list<int>(5);
+    COPY_LIST;
+    CLEAR_LIST;
     END_TX;
-    bool good = dsize == 5;
-    for (int i = 0; i < 5; ++i)
-        good &= (data[i] == 0);
-    for (int i = 5; i < 16; ++i)
-        good &= (data[i] == 17);
-    std::cout << "Sized ctor (2) " << (good ? "passed" : "failed") << std::endl;
-
-
-
-    global_barrier->arrive(id);
-
-
-
-    return;
+    VERIFY("sized ctor (2)", 5, {0, 0, 0, 0, 0, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17});
 
     // test range constructor (3)
+    global_barrier->arrive(id);
     int a[] = {1, 2, 3};
-    my_list = new std::list<int>(a, a+3);
-    check("Expect 1,2,3");
+    BEGIN_TX;
+    // use the range constructor, expect {1, 2, 3}
+    ctor_list = new std::list<int>(a, a+3);
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("range ctor (3)", 3, {1, 2, 3});
 
     // test the copy constructor (4)
-    std::list<int>* old = my_list;
-    my_list = new std::list<int>(*old);
-    delete old;
-    check("Expect 1,2,3");
+    global_barrier->arrive(id);
+    int b[] = {7, 8, 9, 2};
+    if (per_thread_list[id] == NULL) // always true
+        per_thread_list[id] = new std::list<int>({b, b+4});
+    BEGIN_TX;
+    ctor_list = new std::list<int>(*per_thread_list[id]);
+    delete per_thread_list[id];
+    per_thread_list[id] = NULL;
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("copy ctor  (4)", 4, {7, 8, 9, 2});
 
     // test the move constructor (5)
-    old = my_list;
-    my_list = new std::list<int>(std::move(*old));
-    std::list<int>* swapper = my_list;
-    my_list = old;
-    check("Expect empty");
-    my_list = swapper;
-    check("Expect 1, 2, 3");
-    delete old;
-    delete swapper;
+    global_barrier->arrive(id);
+    if (per_thread_list[id] == NULL) // always true
+        per_thread_list[id] = new std::list<int>({a, a+3});
+    int tmp;
+    BEGIN_TX;
+    // Possible GCC bug... nulling the null list before we start causes an
+    // early write, without which we get concurrency errors
+    if (id < 256)
+        ctor_list = NULL;
+    ctor_list = new std::list<int>(std::move(*per_thread_list[id]));
+    tmp = per_thread_list[id]->size(); // expect 0
+    delete per_thread_list[id];
+    per_thread_list[id] = NULL;
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    assert(tmp == 0);
+    VERIFY("move ctor  (5)", 3, {1, 2, 3});
 
     // test the initializer list constructor (6)
-    my_list = new std::list<int>({1, 2, 3, 4, 5, 6});
-    check("Expect 1, 2, 3, 4, 5, 6");
-    delete my_list;
+    global_barrier->arrive(id);
+    BEGIN_TX;
+    ctor_list = new std::list<int>({6, 3, 8, 2, 9, 1});
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("ilist ctor (6)", 6, {6, 3, 8, 2, 9, 1});
 }
