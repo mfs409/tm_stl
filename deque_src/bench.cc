7735d9725c4db94e4c45c4c159782f3ed116e43a
Michael Spear <spear@4pac1.eecs.lehigh.edu>
2014-02-17 14:49:45 -0500

    renamed queue folder, and started a deque folder... we need deque before we can do queue, but right now, dequeue only has part of the table filled out, and doesn't even build

---
+++ deque_src/bench.cc
@@ -0,0 +1,193 @@
+/*
+  Driver to test the transactional version of the std::deque interface
+
+  According to http://www.cplusplus.com/reference/deque/deque/, the
+  std::deque interface consists of the following:
+
+  We use this list for a few purposes.
+
+  ** First, we make sure that every method of std::deque is listed in the
+     Method and # Methods column.  This part is supposed to correlate to what
+     is listed on cplusplus.com.
+
+  ** Second, in Trace Location, we make sure that every instance in the GCC
+     STL source code is listed in Trace Location, with the location of the
+     line of code where the TRACE macro was placed.
+
+  ** Third, in Category, we update VERIFICATION once we've had a successful
+     run in which every TRACE macro for a given category led to the
+     appropriate printf being called.  At that point, we know that every
+     method within the category is now transaction_safe.
+
+|------------------------+---------------+-----------+----------------|
+| Category               | Method        | # Methods | Trace Location |
+| (* if not traced )     |               |           |                |
+|------------------------+---------------+-----------+----------------|
+| Member Functions       | (constructor) |         6 |                |
+| (VERIFICATION: TODO)   | (destructor)  |         1 |                |
+|                        | operator=     |         3 |                |
+|------------------------+---------------+-----------+----------------|
+| Iterators              | begin         |         2 |                |
+| (ctors only)           | end           |         2 |                |
+| (VERIFICATION: TODO)   | rbegin        |         2 |                |
+|                        | rend          |         2 |                |
+|                        | cbegin        |         1 |                |
+|                        | cend          |         1 |                |
+|                        | crbegin       |         1 |                |
+|                        | crend         |         1 |                |
+|------------------------+---------------+-----------+----------------|
+| iterator               |               |           |                |
+| methods: TODO          |               |           |                |
+|------------------------+---------------+-----------+----------------|
+| const_iterator         |               |           |                |
+| methods: TODO          |               |           |                |
+|------------------------+---------------+-----------+----------------|
+| reverse_iterator*      |               |           |                |
+| methods: TODO          |               |           |                |
+|------------------------+---------------+-----------+----------------|
+| const_reverse_iterator |               |           |                |
+| methods: TODO          |               |           |                |
+|------------------------+---------------+-----------+----------------|
+| Capacity               | size          |         1 |                |
+| (VERIFICATION: TODO)   | max_size      |         1 |                |
+|                        | resize        |         2 |                |
+|                        | empty         |         1 |                |
+|                        | shrink_to_fit |         1 |                |
+|------------------------+---------------+-----------+----------------|
+| Element access         | operator[]    |         2 |                |
+| (VERIFICATION: TODO)   | at            |         2 |                |
+|                        | front         |         2 |                |
+|                        | back          |         2 |                |
+|------------------------+---------------+-----------+----------------|
+| Modifiers              | assign        |         3 |                |
+| (VERIFICATION: TODO)   | push_back     |         2 |                |
+|                        | push_front    |         2 |                |
+|                        | pop_back      |         1 |                |
+|                        | pop_front     |         1 |                |
+|                        | insert        |         5 |                |
+|                        | erase         |         2 |                |
+|                        | swap          |         1 |                |
+|                        | clear         |         1 |                |
+|                        | emplace       |         1 |                |
+|                        | emplace_front |         1 |                |
+|                        | emplace_back  |         1 |                |
+|------------------------+---------------+-----------+----------------|
+| Allocators             | get_allocator |         1 |                |
+|------------------------+---------------+-----------+----------------|
+| Non-member             | '=='          |         1 |                |
+| function               | '!='          |         1 |                |
+| overloads (NMFOs)      | '<'           |         1 |                |
+| (7 total... OK)        | '<='          |         1 |                |
+|                        | '>'           |         1 |                |
+|                        | '>='          |         1 |                |
+|                        | swap          |         1 |                |
+|------------------------+---------------+-----------+----------------|
+*/
+
+#include <list>
+#include <cstdio>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <cassert>
+#include <iostream>
+#include <unistd.h>
+
+#include "../common/barrier.h"
+#include "tests.h"
+
+using std::cout;
+using std::endl;
+
+/// configured via command line args: number of threads
+int  num_threads = 1;
+
+/// the barrier to use when we are in concurrent mode
+barrier* global_barrier;
+
+/// the mutex to use when we are in concurrent mode with tm turned off
+std::mutex global_mutex;
+
+/// Report on how to use the command line to configure this program
+void usage()
+{
+    cout << "Command-Line Options:" << endl
+         << "  -n <int> : specify the number of threads" << endl
+         << "  -h       : display this message" << endl << endl;
+    exit(0);
+}
+
+/// Parse command line arguments using getopt()
+void parseargs(int argc, char** argv)
+{
+    // parse the command-line options
+    int opt;
+    while ((opt = getopt(argc, argv, "n:h")) != -1) {
+        switch (opt) {
+          case 'n': num_threads = atoi(optarg); break;
+          case 'h': usage();                    break;
+        }
+    }
+}
+
+/// A concurrent test for exercising every method of std::list.  This is
+/// called by every thread
+void per_thread_test(int id)
+{
+    // wait for all threads to be ready
+    global_barrier->arrive(id);
+
+    // test constructors and destructors
+    ctor_test(id);
+
+    // test assignment operators
+    assign_test(id);
+
+    // test iterators
+    iterator_test(id);
+    reverse_iterator_test(id);
+    legacy_const_iterator_test(id);
+    legacy_const_reverse_iterator_test(id);
+    const_iterator_test(id);
+    const_reverse_iterator_test(id);
+
+    // test capacity
+    cap_test(id);
+
+    // test element access
+    element_test(id);
+
+    // test modifiers
+    modifier_test(id);
+
+    // test operations
+    operations_test(id);
+
+    // test observers
+    observers_test(id);
+
+    // test relational operators
+    relational_test(id);
+
+    // test swap
+    swap_test(id);
+}
+
+/// main() just parses arguments, makes a barrier, and starts threads
+int main(int argc, char** argv)
+{
+    // figure out what we're doing
+    parseargs(argc, argv);
+
+    // set up the barrier
+    global_barrier = new barrier(num_threads);
+
+    // make threads
+    std::thread* threads = new std::thread[num_threads];
+    for (int i = 0; i < num_threads; ++i)
+        threads[i] = std::thread(per_thread_test, i);
+
+    // wait for the threads to finish
+    for (int i = 0; i < num_threads; ++i)
+        threads[i].join();
+}
---
+++ deque_src/ctor.cc
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <cassert>
+#include "tests.h"
+
+/// The list we will use for our tests
+std::list<int>* ctor_list = NULL;
+
+/// An extra array of lists, which is sometimes useful.  Note that while we
+/// treat this as per-thread, it's globally visible, so that the compiler
+/// can't assume anything...
+std::list<int>* per_thread_list[256] = {NULL}; // 256 is way more than enough...
+
+/// clone the list to a local array represented by dsize, data[]
+#define COPY_LIST                                 \
+    dsize = 0;                                    \
+    for (auto i : *ctor_list)                     \
+        data[dsize++] = i
+
+/// destroy the list and set the pointer to NULL
+#define CLEAR_LIST                                \
+    delete ctor_list;                             \
+    ctor_list = NULL
+
+/// compare the local array to our expected results
+#define VERIFY(testName, size, ...)                                     \
+    if (dsize != size)                                                  \
+        printf(" [%d] size did not match %d != %d\n", id, dsize, size);  \
+    else if (size > 0) {                                                \
+        int counter = 0;                                                \
+        bool good = true;                                               \
+        for (auto i : __VA_ARGS__)                                      \
+            good &= (data[counter++] == i);                             \
+        if (!good)                                                      \
+            std::cout << " ["<<id<<"] Arrays did not match for thread " << id << std::endl; \
+        else if (id == 0)                                               \
+            std::cout << " [OK] " << testName << std::endl;              \
+    }                                                                   \
+    else if (id == 0)                                                   \
+            std::cout << " [OK] " << testName << std::endl
+
+/**
+ * Here is the concurrent ctor test.  It also happens to be the concurrent
+ * dtor test, since there is only a basic dtor.
+ */
+void ctor_test(int id)
+{
+    // invariant: the list starts off null
+    assert(ctor_list == NULL);
+
+    // helper message
+    if (id == 0) {
+        std::cout << std::endl
+                  << "Testing ctor and dtor" << std::endl
+                  << "(Note: we aren't testing the allocator parameter "
+                  << "of the constructors)" << std::endl;
+    }
+
+    // scratch space for holding data when computing whether the test
+    // succeeded or failed.
+    int data[32], dsize = 0;
+
+    // test basic constructor (1)
+    global_barrier->arrive(id);
+    BEGIN_TX;
+    // call the basic constructor, expect empty list
+    ctor_list = new std::list<int>();
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("basic ctor (1)", 0, {0});
+
+    // test sized constructor (2)
+    global_barrier->arrive(id);
+    // make sure we'll detect errors in data[]
+    for (int i = 0; i < 16; ++i)
+        data[i] = 17;
+    BEGIN_TX;
+    // call the sized constructor, expect {0,0,0,0,0}
+    // NB: we padded data[] to detect overflow
+    ctor_list = new std::list<int>(5);
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("sized ctor (2a)", 5, {0, 0, 0, 0, 0, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17});
+
+    // test sized constructor (2b)
+    global_barrier->arrive(id);
+    // make sure we'll detect errors in data[]
+    for (int i = 0; i < 16; ++i)
+        data[i] = 17;
+    BEGIN_TX;
+    // call the sized constructor, expect {0,0,0,0,0}
+    // NB: we padded data[] to detect overflow
+    ctor_list = new std::list<int>(5, 9);
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("sized ctor (2b)", 5, {9, 9, 9, 9, 9, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17});
+
+    // test range constructor (3)
+    global_barrier->arrive(id);
+    int a[] = {1, 2, 3};
+    BEGIN_TX;
+    // use the range constructor, expect {1, 2, 3}
+    ctor_list = new std::list<int>(a, a+3);
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("range ctor (3)", 3, {1, 2, 3});
+
+    // test the copy constructor (4)
+    global_barrier->arrive(id);
+    int b[] = {7, 8, 9, 2};
+    if (per_thread_list[id] == NULL) // always true
+        per_thread_list[id] = new std::list<int>({b, b+4});
+    BEGIN_TX;
+    ctor_list = new std::list<int>(*per_thread_list[id]);
+    delete per_thread_list[id];
+    per_thread_list[id] = NULL;
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("copy ctor  (4)", 4, {7, 8, 9, 2});
+
+    // test the move constructor (5)
+    global_barrier->arrive(id);
+    if (per_thread_list[id] == NULL) // always true
+        per_thread_list[id] = new std::list<int>({a, a+3});
+    int tmp;
+    BEGIN_TX;
+    // Possible GCC bug... nulling the null list before we start causes an
+    // early write, without which we get concurrency errors
+    if (id < 256)
+        ctor_list = NULL;
+    ctor_list = new std::list<int>(std::move(*per_thread_list[id]));
+    tmp = per_thread_list[id]->size(); // expect 0
+    delete per_thread_list[id];
+    per_thread_list[id] = NULL;
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    assert(tmp == 0);
+    VERIFY("move ctor  (5)", 3, {1, 2, 3});
+
+    // test the initializer list constructor (6)
+    global_barrier->arrive(id);
+    BEGIN_TX;
+    ctor_list = new std::list<int>({6, 3, 8, 2, 9, 1});
+    COPY_LIST;
+    CLEAR_LIST;
+    END_TX;
+    VERIFY("ilist ctor (6)", 6, {6, 3, 8, 2, 9, 1});
+
+    global_barrier->arrive(id);
+}
