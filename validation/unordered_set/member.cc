ea0304113e924c6b1d1d1681464d2c2dbbaa0c56
Chao Wang <albertwang87@gmail.com>
2014-08-21 12:30:09 -0400

    member function DONE

--- validation/unordered_set/member.cc
+++ validation/unordered_set/member.cc
@@ -29,20 +29,6 @@ void ctor_dtor_tests(int id)
     }
 
      
-    // the simple initializer list constructor (5a)
-    global_barrier->arrive(id);
-    {
-        verifier v;
-        BEGIN_TX;
-        member_unordered_set = new std::unordered_set<int>({1, 2, 3, 4, 100, 99, 98, 97});
-        v.insert_all<std::unordered_set<int>>(member_unordered_set);
-        delete(member_unordered_set);
-        member_unordered_set = NULL;
-        END_TX;
-	
-        v.check("initializer list ctor (5a)", id, 8,
-              { 1, 2, 3, 4, 100, 99, 98, 97, -2});
-    }
 
     // the range constructor (2a)
     global_barrier->arrive(id);
@@ -95,8 +81,56 @@ void ctor_dtor_tests(int id)
 
         v.check_size("the copy constructor (3b)", id, size);
     }
+
+    // the move constructor (4b)
+    global_barrier->arrive(id);
+    {
+	verifier v;
+	int size;
+	BEGIN_TX;
+	std::unordered_set<int> tmp({1,2,3,4,9,8,7,6});
+	auto a = tmp.get_allocator();
+	member_unordered_set = new std::unordered_set<int>(std::move(tmp), a);
+	size = member_unordered_set->size();
+	v.insert_all<std::unordered_set<int>>(member_unordered_set);
+	delete(member_unordered_set);
+	member_unordered_set = NULL;
+	END_TX;
+
+	v.check_size("the move constructor (4b)", id, size);
+    }
+
+    // the simple initializer list constructor (5a)
+    global_barrier->arrive(id);
+    {
+        verifier v;
+        BEGIN_TX;
+        member_unordered_set = new std::unordered_set<int>({1, 2, 3, 4, 100, 99, 98, 97});
+        v.insert_all<std::unordered_set<int>>(member_unordered_set);
+        delete(member_unordered_set);
+        member_unordered_set = NULL;
+        END_TX;
+	
+        v.check("initializer list ctor (5a)", id, 8,
+              { 1, 2, 3, 4, 100, 99, 98, 97, -2});
+    }
 }
 
 void op_eq_tests(int id)
 {
+    global_barrier->arrive(id);
+    {
+	verifier v;
+	int size;
+	BEGIN_TX;
+	member_unordered_set = new std::unordered_set<int>();
+	*member_unordered_set = {1,2,3,4,9,8,7,6};
+	size = member_unordered_set->size();
+	v.insert_all<std::unordered_set<int>>(member_unordered_set);
+	delete(member_unordered_set);
+	member_unordered_set = NULL;
+	END_TX;
+
+	v.check_size("initializer list operator= (3)", id, size);
+    }
 }
