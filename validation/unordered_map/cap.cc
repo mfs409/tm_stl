1e64ce8586c8c8a6bb9e9bfbd84ca15bcb6744b9
Yujie Liu <yujie.liu@gmail.com>
2014-08-23 17:43:42 -0400

    complete cap.cc

--- validation/unordered_map/cap.cc
+++ validation/unordered_map/cap.cc
@@ -1,3 +1,55 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cassert>
+#include "tests.h"
+#include "verify.h"
+
+/// The map we will use for our tests
+typedef std::unordered_map<int, int> intmap;
+typedef std::pair<int, int>          intpair;
+typedef map_verifier verifier;
+
+static intmap* member_map = NULL;
+
 void cap_tests(int id)
 {
+    // test empty
+    global_barrier->arrive(id);
+    {
+        int v;
+        BEGIN_TX;
+        member_map = new intmap();
+        v = member_map->empty();
+        delete(member_map);
+        member_map = NULL;
+        END_TX;
+        std::cout << "  [OK] empty = " << v << std::endl;
+    }
+
+    // test size
+    global_barrier->arrive(id);
+    {
+        int v;
+        BEGIN_TX;
+        member_map = new intmap();
+        v = member_map->size();
+        delete(member_map);
+        member_map = NULL;
+        END_TX;
+        std::cout << "  [OK] size = " << v << std::endl;
+    }
+
+    // test max size
+    global_barrier->arrive(id);
+    {
+        int v;
+        BEGIN_TX;
+        member_map = new intmap();
+        v = member_map->max_size();
+        delete(member_map);
+        member_map = NULL;
+        END_TX;
+        std::cout << "  [OK] max_size = " << v << std::endl;
+    }
 }
