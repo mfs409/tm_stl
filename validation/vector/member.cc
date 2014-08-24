21e1191ba29a23716c2c09a61cd305acedebf0ac
Tingzhe <tiz214@lehigh.edu>
2014-08-24 18:30:17 -0400

    Vector

--- validation/vector/member.cc
+++ validation/vector/member.cc
@@ -70,8 +70,114 @@ void ctor_dtor_tests(int id)
         v.check("fill ctor (2a) and fill ctor (2b)", id, 8,
               { 0, 0, 0, 0, 98, 98, 98, 98, -2});
     }
+
+    // the next test will use the range ctor
+    global_barrier->arrive(id);
+    {
+        verifier v;
+        int q [] = { 9, 8, 7 };
+        BEGIN_TX;
+        member_vec = new std::vector<int>(q, q+3);
+        v.insert_all<std::vector<int>>(member_vec);
+        delete(member_vec);
+        member_vec = NULL;
+        END_TX;
+        v.check("range ctor (3)", id, 3, { 9, 8, 7, -2 });
+   }
+   // the next test will use the copy ctor
+    global_barrier->arrive(id);
+    {
+        verifier v;
+        std::vector<int> local = { 3, 4, 5, 6 };
+        BEGIN_TX;
+        member_vec = new std::vector<int>(local);
+        v.insert_all<std::vector<int>>(member_vec);
+        delete(member_vec);
+        member_vec = NULL;
+        END_TX;
+        v.check("copy ctor (4a)", id, 4, {3, 4, 5, 6, -2});
+    }
+    
+    // the next test is the move ctor
+    global_barrier->arrive(id);
+    {
+        verifier v;
+        std::vector<int> local = {5, 4, 3};
+        BEGIN_TX;
+        member_vec = new std::vector<int>(std::move(local));
+        v.insert_all<std::vector<int>>(member_vec);
+        delete(member_vec);
+        member_vec = NULL;
+        END_TX;
+        v.check("move ctor (5a)", id, 3, {5, 4, 3, -2});
+    }
+    // the next test is the ilist ctor
+    global_barrier->arrive(id);
+    {
+        verifier v;
+        BEGIN_TX;
+        member_vec = new std::vector<int>({11, 13, 15, 17});
+        v.insert_all<std::vector<int>>(member_vec);
+        delete(member_vec);
+        member_vec = NULL;
+        END_TX;
+        v.check("move ilist ctor (6)", id, 4, {11, 13, 15, 17, -2});
+    }
 }
 
 void op_eq_tests(int id)
 {
+    global_barrier->arrive(id);
+
+    // a temporary array into which we can copy deque data
+    int data[256], dsize;
+
+    if (id == 0)
+        printf("Testing operator= (3)\n");
+
+    // test #1 is operator= copy
+    //
+    // NB: we haven't actually verified iterators yet, but we use them...
+ /* This function hasn't been implementation
+   global_barrier->arrive(id);
+    {
+        verifier v;
+        std::vector<int> local = { 3, 4, 5, 6 };
+        BEGIN_TX;
+        member_vec = new std::vector<int>();
+        *member_vec = local;
+        v.insert_all<std::vector<int>>(member_vec);
+        delete(member_vec);
+        member_vec = NULL;
+        END_TX;
+        v.check("copy operator= (1)", id, 4, {3, 4, 5, 6, -2});
+    }*/
+    // test #2 is operator= move
+    global_barrier->arrive(id);
+    {
+        verifier v;
+        std::vector<int> local = { 9, 8, 7, 6 };
+        BEGIN_TX;
+        member_vec = new std::vector<int>();
+        *member_vec = std::move(local);
+        v.insert_all<std::vector<int>>(member_vec);
+        delete(member_vec);
+        member_vec = NULL;
+        END_TX;
+        v.check("move operator= (2)", id, 4, {9, 8, 7, 6, -2});
+    }
+
+    // test #3 is operator= ilist
+    global_barrier->arrive(id);
+    {
+        verifier v;
+        BEGIN_TX;
+        member_vec = new std::vector<int>();
+        *member_vec = { 13, 14, 15, 16 };
+        v.insert_all<std::vector<int>>(member_vec);
+        delete(member_vec);
+        member_vec = NULL;
+        END_TX;
+        v.check("ilist operator= (3)", id, 4, {13, 14, 15, 16, -2});
+    }
 }
