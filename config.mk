dcbf6190ad98b6bb2abeee3e5a225f67893bdaaa
Michael Spear <spear@cse.lehigh.edu>
2014-10-14 17:45:21 -0400

    updated build to gcc 5.0.0 experimental

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
 #
-# You must have gcc 4.10 (experimental) built and installed. This variable
+# You must have gcc 5.0 (experimental) built and installed. This variable
 # describes the location of that installation.
 #
 # As a sanity check:
-# $(GCC410INSTALL)/lib/gcc/x86_64-unknown-linux-gnu/4.10.0/include should
+# $(GCC5INSTALL)/lib/gcc/x86_64-unknown-linux-gnu/5.0.0/include should
 # have stddef.h in it
 #
-GCC410INSTALL := /home/spear/gcc410/install
+GCC5INSTALL := /home/spear/gcc5/install
